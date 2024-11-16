6084052fa9615715421736df479009034fef9ebd
Racha Bharadwaj <rachabharadwajgupta@gmail.com>
2024-11-16 12:36:02 +0530

    Initial commit

---
+++ src/components/DashboardPage/DashNav/index.jsx
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import "./DashNav.styles.css";
+import userImg from "../../../assets/images/userImg.png";
+import { DashContext } from "..";
+import { FetchedContext } from "../../../App";
+
+const DashNav = () => {
+
+  const {isDash, setIsDash} = useContext(DashContext);
+  const {openUserAccount, setOpenUserAccount} = useContext(FetchedContext);
+
+  const dashClick = ()=>{
+    setIsDash(true)
+  }
+  const taskClick = ()=>{
+    setIsDash(false)
+  }
+  return (
+    <div className="dash-nav">
+      <div className="dash-logo">
+        TodoList
+      </div>
+      {/* Dash/Tasks Toggler */}
+      <div className="dash-tasks-togg">
+        <div className={`dash-tog-btn tasks-btn ${isDash? "": "active-toggle"}`} onClick={taskClick} >
+          Tasks
+        </div>
+      </div>
+
+      <div className="user-account-name">
+        {/* For Responsiveness */}
+        <div className="user-img-state" onClick={()=>{
+          setOpenUserAccount(!openUserAccount);
+        }}>
+          <img className="user-img-img" src={userImg} alt="" />
+        </div>
+        {/* For Regular Use */}
+        <div className="user-img">
+        <img className="user-img" src={userImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashNav;
